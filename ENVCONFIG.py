10be7fb63e29a17036df49a03f70dcdd53ce8595
William McCorkindale <wjm41@cam.ac.uk>
2022-01-26 22:21:47 +0000

    included chinese-simplified fonts & improved GAN

--- ENVCONFIG.py
+++ ENVCONFIG.py
@@ -1,22 +1,26 @@
 import os
 import sys
 
-repolocation=os.path.abspath(os.path.dirname(__file__))
-home=os.path.expanduser("~")
+repolocation = os.path.abspath(os.path.dirname(__file__))
+home = os.path.expanduser("~")
 
 #######################################################################################################
-### Configuration definitions [ADD YOUR NEW CONFIGURATION HERE!]
+# Configuration definitions [ADD YOUR NEW CONFIGURATION HERE!]
 
-### Automatically tries to determine config from repolocation
+# Automatically tries to determine config from repolocation
 print(repolocation)
-if repolocation=='/eos/home-d/drousso/SWAN_projects/Other/GANji':
-    CONFIG='DAVID'
+if repolocation == '/eos/home-d/drousso/SWAN_projects/Other/GANji':
+    CONFIG = 'DAVID'
+elif repolocation == '/home/wjm41/ml_physics/GANji':
+    CONFIG = 'WILL-CSD3'
 else:
-    CONFIG='WILL'
+    CONFIG = 'bruh'
 print(CONFIG)
 
-### Configuration definitions
-if CONFIG=="DAVID":
+# Configuration definitions
+if CONFIG == "DAVID":
     rds_dir = '/eos/home-d/drousso/SWAN_projects/Other'
-elif CONFIG=="WILL":
+elif CONFIG == "WILL-CSD3":
     rds_dir = '/rds-d2/user/wjm41/hpc-work/datasets'
+else:
+    print('ur a noob')
